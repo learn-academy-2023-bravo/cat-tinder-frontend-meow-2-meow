08f8be6ea8eb600290838c6b349f3ff836632a1a
Jose Luis Salas Jr <125709745+salas89jl@users.noreply.github.com>
2023-05-08 09:30:42 -0700

    added carousel component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import CarouselNav from "../components/Carousel"
 
 
 const Home = () => {
@@ -7,9 +8,18 @@ const Home = () => {
     <h1>Welcome to Meet Meow</h1>
 
   <br />
-    <div className="p-home">“Are you tired of swiping left on all the basic tabbies and predictable persians? Well, look no further! Cat Tinder is here to help you find the purrrfect match. Whether you're a sassy Siamese or a cool and collected Russian Blue, we've got the feline for you. So don't be a scaredy-cat, swipe right on Cat Tinder and find your meow-mate today!”
+    <div className="p-home">
+      <p>
+        “Are you tired of swiping left on all the basic tabbies and predictable persians? Well, look no further! Cat Tinder is here to help you find the purrrfect match. Whether you're a sassy Siamese or a cool and collected Russian Blue, we've got the feline for you. So don't be a scaredy-cat, swipe right on Cat Tinder and find your meow-mate today!”
+      </p>
+      <span className="home-carousel">
+    <CarouselNav />
+    </span>
     </div>
+  <br />
+  
   </div>
+  
 
 </>
 
