77c40edd261d4e0e0d402c04986e65fe9e9c4d3b
Ealvarez01 <e.alvarez2404@yahoo.com>
2023-05-08 16:16:27 -0700

    comments deleted as requested

--- src/_test_/Home.test.js
+++ src/_test_/Home.test.js
@@ -10,9 +10,5 @@ describe('<Home />', () => {
 
         const p_home = screen.getByText(/are you tired of swiping left on all the basic tabbies and predictable persians\? well, look no further! cat tinder is here to help you find the purrrfect match\. whether you're a sassy siamese or a cool and collected russian blue, we've got the feline for you\. so don't be a scaredy\-cat, swipe right on cat tinder and find your meow\-mate today!/i)
         expect(p_home).toBeInTheDocument()
-
-        // const element = screen.getByText("Welcome to Meet Meow")
-        // expect(element).toBeInTheDocument()
-        // screen.logTestingPlaygroundURL()
     })
 })
